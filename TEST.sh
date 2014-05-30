c0747be41be6ca229222b00c4b0e6eceff444df2
Axel Viala <axel.viala@darnuria.eu>
2014-05-30 03:16:53 +0200

    Removing TEST.sh

--- TEST.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mkdir -p bin
-RUST_BACKTRACE=1 rustc --test -g -o bin/test src/vodk/main.rs -L extern/glfw-rs/lib -L extern/gl-rs/lib -C link-args="-lglfw -lrt -lXrandr -lXi -lGL -lm -ldl -lXrender -ldrm -lXdamage -lX11-xcb -lxcb-glx -lxcb-dri2 -lXxf86vm -lXfixes -lXext -lX11 -lpthread -lxcb -lXau" && ./bin/test
