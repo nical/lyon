7e7dc7d66b1e769f47b03321c7032fd483f80ca4
Nicolas Silva <nical.silva@gmail.com>
2015-03-31 09:37:39 +0200

    kiwi: use vodk_id.

--- id/src/id_vector.rs
+++ id/src/id_vector.rs
@@ -1,4 +1,4 @@
-use super::{ Identifier };
+use super::{ Identifier, FromIndex };
 use std::default::Default;
 use std::slice;
 use std::marker::PhantomData;
@@ -36,9 +36,19 @@ impl<ID:Identifier, Data> IdVector<ID, Data> {
 
     pub fn len(&self) -> usize { self.data.len() }
 
+    pub fn at_index(&self, idx: usize) -> &Data { &self.data[idx] }
+
+    pub fn at_index_mut(&mut self, idx: usize) -> &mut Data { &mut self.data[idx] }
+
     pub fn iter<'l>(&'l self) -> slice::Iter<'l, Data> { self.data.iter() }
 
     pub fn iter_mut<'l>(&'l mut self) -> slice::IterMut<'l, Data> { self.data.iter_mut() }
+
+    pub fn push(&mut self, elt: Data) -> ID {
+        let index = self.data.len();
+        self.data.push(elt);
+        return FromIndex::from_index(index);
+    }
 
     pub fn clear(&mut self) {
         self.data.clear();
--- kiwi/src/graph.rs
+++ kiwi/src/graph.rs
@@ -1,13 +1,17 @@
 use std::rc::Rc;
 use std::default::Default;
 use std::slice;
+use std::ops;
 
-type DataTypeList = Vec<DataTypeID>;
+use vodk_id::*;
+use vodk_id::id_vector::IdVector;
+
+type DataTypeList = Vec<DataTypeId>;
 
 #[derive(Copy, PartialEq, Debug)]
 enum DataType {
     Generic(u32),
-    Type(DataTypeID),
+    Type(DataTypeId),
 }
 
 #[derive(Copy, Debug)]
@@ -24,48 +28,58 @@ struct NodeDescriptor {
 struct Node {
     inputs: Vec<Connection>,
     outputs: Vec<Connection>,
-    generics: Vec<Option<DataTypeID>>,
-    node_type: NodeTypeID,
+    generics: Vec<Option<DataTypeId>>,
+    node_type: NodeTypeId,
     valid: bool,
 }
 
 #[derive(Copy, Debug)]
 struct Connection {
     port: u16,
-    other_node: u16,
+    other_node: NodeId,
     other_port: u16,
 }
 
 struct TypeSystem {
-    descriptors: Vec<NodeDescriptor>,
+    descriptors: IdVector<NodeTypeId, NodeDescriptor>,
 }
 
 struct Graph {
-    nodes: Vec<Node>,
+    nodes: IdVector<NodeId, Node>,
     type_system: Rc<TypeSystem>,
 }
 
 type PortIndex = u16;
 type PortID = u16; // TODO
 
-#[derive(Copy, PartialEq, Clone, Debug)]
-struct NodeID { handle: u16 }
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Node_;
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct NodeType_;
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct DataType_;
 
-#[derive(Copy, PartialEq, Clone, Debug)]
-struct NodeTypeID { handle: i32 }
+pub type NodeHandle = u16;
+pub type NodeTypeHandle = u16;
+pub type DataTypeHandle = u32;
 
-#[derive(Copy, PartialEq, Clone, Debug)]
-struct DataTypeID { handle: u32 }
+pub type NodeId = Id<Node_, NodeHandle>;
+pub type NodeTypeId = Id<NodeType_, NodeTypeHandle>;
+pub type DataTypeId = Id<DataType_, DataTypeHandle>;
+
+pub fn node_id(index: NodeHandle) -> NodeId { NodeId::new(index) }
+pub fn node_type_id(index: NodeTypeHandle) -> NodeTypeId { NodeTypeId::new(index) }
+pub fn data_type_id(index: DataTypeHandle) -> DataTypeId { DataTypeId::new(index) }
 
 impl Graph {
     pub fn new(type_system: Rc<TypeSystem>) -> Graph {
         Graph {
-            nodes: Vec::new(),
+            nodes: IdVector::new(),
             type_system: type_system,
         }
     }
 
-    pub fn connect(&mut self, n1: NodeID, p1: PortIndex, n2: NodeID, p2: PortIndex) -> bool {
+    pub fn connect(&mut self, n1: NodeId, p1: PortIndex, n2: NodeId, p2: PortIndex) -> bool {
         if self.are_connected(n1, p1, n2, p2) {
             return true;
         }
@@ -78,10 +92,10 @@ impl Graph {
         }
         let c_type = c_type.unwrap();
         {
-            let mut node1 = &mut self.nodes[n1.handle as usize];
+            let mut node1 = &mut self.nodes[n1];
             node1.outputs.push(Connection {
                 port: p1,
-                other_node: n2.handle,
+                other_node: n2,
                 other_port: p2
             });
             node1.outputs.sort_by(|a,b|{a.port.cmp(&b.port)});
@@ -91,10 +105,10 @@ impl Graph {
             }
         }
         {
-            let mut node2 = &mut self.nodes[n2.handle as usize];
+            let mut node2 = &mut self.nodes[n2];
             node2.inputs.push(Connection {
                 port: p2,
-                other_node: n1.handle,
+                other_node: n1,
                 other_port: p1,
             });
             node2.inputs.sort_by(|a,b|{a.port.cmp(&b.port)});
@@ -107,25 +121,24 @@ impl Graph {
         return true;
     }
 
-    pub fn are_connected(&self, n1: NodeID, p1: PortIndex, n2: NodeID, p2: PortIndex) -> bool {
-        if self.nodes.len() <= n1.handle as usize
-            || self.nodes.len() <= n2.handle as usize {
+    pub fn are_connected(&self, n1: NodeId, p1: PortIndex, n2: NodeId, p2: PortIndex) -> bool {
+        if self.nodes.len() <= n1.handle as usize || self.nodes.len() <= n2.handle as usize {
             return false;
         }
 
-        let node1 = &self.nodes[n1.handle as usize];
-        let node2 = &self.nodes[n2.handle as usize];
+        let node1 = &self.nodes[n1];
+        let node2 = &self.nodes[n2];
 
         let mut connected1 = false;
         for p in node1.outputs.iter() {
-            if p.port == p1 && p.other_node == n2.handle && p.other_port == p2 {
+            if p.port == p1 && p.other_node == n2 && p.other_port == p2 {
                 connected1 = true;
             }
         }
 
         let mut connected2 = false;
         for p in node2.inputs.iter() {
-            if p.port == p2 && p.other_node == n1.handle && p.other_port == p1 {
+            if p.port == p2 && p.other_node == n1 && p.other_port == p1 {
                 connected2 = true;
             }
         }
@@ -135,11 +148,11 @@ impl Graph {
         return connected1;
     }
 
-    fn get_node<'l>(&'l self, id: NodeID) -> &'l Node {
-        return &self.nodes[id.handle as usize];
+    fn get_node<'l>(&'l self, id: NodeId) -> &'l Node {
+        return &self.nodes[id];
     }
 
-    pub fn can_connect(&self, n1: NodeID, p1: PortIndex, n2: NodeID, p2: PortIndex) -> bool {
+    pub fn can_connect(&self, n1: NodeId, p1: PortIndex, n2: NodeId, p2: PortIndex) -> bool {
         if !self.contains(n1) || !self.contains(n2) {
             return false;
         }
@@ -149,60 +162,57 @@ impl Graph {
         ).is_some();
     }
 
-    pub fn disconnect_input(&mut self, n: NodeID, p: PortID) {
-        let n_handle = n.handle as usize;
-        if !self.nodes[n_handle].valid {
+    pub fn disconnect_input(&mut self, n: NodeId, p: PortID) {
+        if !self.nodes[n].valid {
             return;
         }
         // look for the connections in n's inputs
         let mut i = 0;
         loop {
-            if i >= self.nodes[n_handle].inputs.len() {
+            if i >= self.nodes[n].inputs.len() {
                 break;
             }
-            let inputs_i = self.nodes[n_handle].inputs[i];
+            let inputs_i = self.nodes[n].inputs[i];
             if inputs_i.port == p {
-                let input_node = inputs_i.other_node as usize;
+                let input_node = inputs_i.other_node;
                 {
                     let outputs = &mut self.nodes[input_node].outputs;
                     // look for the corresponding connection in the othe node's outputs
                     let mut j = 0;
                     loop {
-                        if outputs[j].other_node == n.handle
-                            && outputs[j].other_port == p {
+                        if outputs[j].other_node == n && outputs[j].other_port == p {
                             outputs.remove(j);
                             break;
                         }
                         j += 1;
                     }
                 }
-                self.nodes[n_handle].inputs.remove(i);
+                self.nodes[n].inputs.remove(i);
             } else {
                 i += 1;
             }
         }
     }
 
-    pub fn disconnect_output(&mut self, n: NodeID, p: PortID) {
-        let n_handle = n.handle as usize;
-        if !self.nodes[n_handle].valid {
+    pub fn disconnect_output(&mut self, n: NodeId, p: PortID) {
+        if !self.nodes[n].valid {
             return;
         }
         // look for the connections in n's outputs
         let mut i = 0;
         loop {
-            if i >= self.nodes[n_handle].outputs.len() {
+            if i >= self.nodes[n].outputs.len() {
                 break;
             }
-            let outputs_i = self.nodes[n_handle].outputs[i];
+            let outputs_i = self.nodes[n].outputs[i];
             if outputs_i.port == p {
-                let output_node = outputs_i.other_node as usize;
+                let output_node = outputs_i.other_node;
                 {
                     let inputs = &mut self.nodes[output_node].inputs;
                     // look for the corresponding connection in the othe node's outputs
                     let mut j = 0;
                     loop {
-                        if inputs[j].other_node == n.handle
+                        if inputs[j].other_node == n
                             && inputs[j].other_port == p {
                             inputs.remove(j);
                             break;
@@ -210,58 +220,68 @@ impl Graph {
                         j += 1;
                     }
                 }
-                self.nodes[n_handle].outputs.remove(i);
+                self.nodes[n].outputs.remove(i);
             } else {
                 i += 1;
             }
         }
     }
 
-    pub fn add(&mut self, id: NodeTypeID) -> NodeID {
+    pub fn add(&mut self, id: NodeTypeId) -> NodeId {
         let mut i = 0;
         for ref n in self.nodes.iter() {
             if !n.valid { break; }
             i += 1;
         }
-        if i == self.nodes.len() {
-            self.nodes.push(Node::new(id));
+        return if i == self.nodes.len() {
+            self.nodes.push(Node::new(id))
         } else {
-            self.nodes[i] = Node::new(id);
-        }
-        return NodeID { handle: i as u16 };
+            *self.nodes.at_index_mut(i) = Node::new(id);
+            node_id(i as u16)
+        };
     }
 
-    pub fn remove(&mut self, id: NodeID) {
+    pub fn remove(&mut self, id: NodeId) {
         if !self.contains(id) {
             return;
         }
         loop {
-            if self.nodes[id.handle as usize].inputs.len() > 0 {
+            if self.nodes[id].inputs.len() > 0 {
                 self.disconnect_input(id, 0);
             } else {
                 break;
             }
         }
         loop {
-            if self.nodes[id.handle as usize].outputs.len() > 0 {
+            if self.nodes[id].outputs.len() > 0 {
                 self.disconnect_output(id, 0);
             } else {
                 break;
             }
         }
-        self.nodes[id.handle as usize].valid = false;
+        self.nodes[id].valid = false;
     }
 
-    pub fn contains(&self, id: NodeID) -> bool {
+    pub fn contains(&self, id: NodeId) -> bool {
         if self.nodes.len() <= id.handle as usize {
             return false;
         }
-        return self.nodes[id.handle as usize].valid;
+        return self.nodes[id].valid;
     }
 }
 
+//impl ops::Index<NodeId> for Graph {
+//    type Output = Node;
+//    fn index<'l>(&'l self, id: &NodeId) -> &'l Node { &self.nodes[*id] }
+//}
+//
+//impl ops::IndexMut<NodeId> for Graph {
+//    fn index_mut<'l>(&'l mut self, id: &NodeId) -> &'l mut Node { &mut self.nodes[*id] }
+//}
+
+
 impl Node {
-    fn new(t: NodeTypeID) -> Node {
+    fn new(t: NodeTypeId) -> Node {
         Node {
             inputs: Vec::new(),
             outputs: Vec::new(),
@@ -275,27 +295,26 @@ impl Node {
 impl TypeSystem {
     pub fn new() -> TypeSystem {
         TypeSystem {
-            descriptors: Vec::new(),
+            descriptors: IdVector::new(),
         }
     }
 
-    pub fn add(&mut self, desc: NodeDescriptor) -> NodeTypeID {
+    pub fn add(&mut self, desc: NodeDescriptor) -> NodeTypeId {
         if !desc.is_valid() { panic!("Invalid node descriptor"); }
-        self.descriptors.push(desc);
-        return NodeTypeID { handle: self.descriptors.len() as i32 - 1 };
+        return self.descriptors.push(desc);
     }
 
-    pub fn get<'l>(&'l self, type_id: NodeTypeID) -> &'l NodeDescriptor {
-        return &self.descriptors[type_id.handle as usize];
+    pub fn get<'l>(&'l self, type_id: NodeTypeId) -> &'l NodeDescriptor {
+        return &self.descriptors[type_id];
     }
 
     pub fn can_connect_types(
         &self,
-        o_node: NodeTypeID, o_port: PortIndex,
-        i_node: NodeTypeID, i_port: PortIndex
+        o_node: NodeTypeId, o_port: PortIndex,
+        i_node: NodeTypeId, i_port: PortIndex
     ) -> bool {
-        let o_types = self.get(o_node).get_output_types(o_port);
-        let i_types = self.get(i_node).get_input_types(i_port);
+        let o_types = self[o_node].get_output_types(o_port);
+        let i_types = self[i_node].get_input_types(i_port);
         return intersect_types(i_types, o_types).len() > 0;
     }
 
@@ -303,9 +322,9 @@ impl TypeSystem {
         &self,
         o_node: &Node, o_port: PortIndex,
         i_node: &Node, i_port: PortIndex
-    ) -> Option<DataTypeID> {
-        let o_desc = self.get(o_node.node_type);
-        let i_desc = self.get(i_node.node_type);
+    ) -> Option<DataTypeId> {
+        let o_desc = &self[o_node.node_type];
+        let i_desc = &self[i_node.node_type];
 
         let o_types = self.get_output_types(o_node, o_desc, o_port);
         let i_types = self.get_input_types(i_node, i_desc, i_port);
@@ -319,7 +338,7 @@ impl TypeSystem {
         &self, node: &'l Node,
         desc: &'l NodeDescriptor,
         port: PortIndex
-    ) -> &'l [DataTypeID] {
+    ) -> &'l [DataTypeId] {
         if desc.inputs.len() <= port as usize {
             return &[];
         }
@@ -340,7 +359,7 @@ impl TypeSystem {
         &self, node: &'l Node,
         desc: &'l NodeDescriptor,
         port: PortIndex
-    ) -> &'l [DataTypeID] {
+    ) -> &'l [DataTypeId] {
         if desc.outputs.len() <= port as usize {
             return &[];
         }
@@ -358,9 +377,18 @@ impl TypeSystem {
     }
 }
 
+impl ops::Index<NodeTypeId> for TypeSystem {
+    type Output = NodeDescriptor;
+    fn index<'l>(&'l self, id: &NodeTypeId) -> &'l NodeDescriptor { &self.descriptors[*id] }
+}
+
+impl ops::IndexMut<NodeTypeId> for TypeSystem {
+    fn index_mut<'l>(&'l mut self, id: &NodeTypeId) -> &'l mut NodeDescriptor { &mut self.descriptors[*id] }
+}
+
 impl NodeDescriptor {
 
-    fn get_input_types<'l>(&'l self, port: PortIndex) -> &'l [DataTypeID] {
+    fn get_input_types<'l>(&'l self, port: PortIndex) -> &'l [DataTypeId] {
         if port as usize >= self.inputs.len() {
             return &[];
         }
@@ -370,7 +398,7 @@ impl NodeDescriptor {
         }
     }
 
-    fn get_output_types<'l>(&'l self, port: PortIndex) -> &'l [DataTypeID] {
+    fn get_output_types<'l>(&'l self, port: PortIndex) -> &'l [DataTypeId] {
         if port as usize >= self.outputs.len() {
             return &[];
         }
@@ -405,54 +433,11 @@ impl NodeDescriptor {
     }
 }
 
-struct NodeAttributeVector<T> {
-    data: Vec<T>
-}
-
-impl<T: Default> NodeAttributeVector<T> {
-
-    pub fn new() -> NodeAttributeVector<T> {
-        NodeAttributeVector {
-            data: Vec::new()
-        }
-    }
-
-    pub fn set(&mut self, id: NodeID, val: T) {
-        while self.data.len() <= id.handle as usize {
-            self.data.push(Default::default());
-        }
-        self.data[id.handle as usize] = val;
-    }
-
-    pub fn get<'l>(&'l self, id: NodeID) -> &'l T {
-        return &self.data[id.handle as usize];
-    }
-
-    pub fn get_mut<'l> (&'l mut self, id: NodeID) -> &'l mut T {
-        while self.data.len() <= id.handle as usize {
-            self.data.push(Default::default());
-        }
-        return &mut self.data[id.handle as usize];
-    }
-
-    pub fn erase(&mut self, id: NodeID) {
-        if self.data.len() <= id.handle as usize {
-            return;
-        }
-
-        self.data[id.handle as usize] = Default::default();
-    }
-
-    pub fn len(&self) -> usize { self.data.len() }
-
-    pub fn clear(&mut self) { self.data.clear(); }
-}
-
 #[cfg(test)]
 mod tests {
     use super::{
-        Graph, NodeDescriptor, DataTypeID,
-        TypeSystem, PortDescriptor, DataType,
+        Graph, NodeDescriptor,
+        TypeSystem, PortDescriptor, DataType, data_type_id,
     };
     use std::rc::Rc;
 
@@ -460,8 +445,8 @@ mod tests {
     fn simple_graph() {
         let mut types = TypeSystem::new();
 
-        let INT = DataTypeID { handle: 0};
-        let FLOAT = DataTypeID { handle: 1};
+        let INT = data_type_id(0);
+        let FLOAT = data_type_id(1);
 
         let t1 = types.add(NodeDescriptor {
             generics: Vec::new(),
@@ -534,8 +519,8 @@ mod tests {
     }
 }
 
-fn intersect_types(a: &[DataTypeID], b:&[DataTypeID]) -> Vec<DataTypeID> {
-    let mut result: Vec<DataTypeID> = Vec::new();
+fn intersect_types(a: &[DataTypeId], b:&[DataTypeId]) -> Vec<DataTypeId> {
+    let mut result: Vec<DataTypeId> = Vec::new();
     for i in a.iter() {
         for j in b.iter() {
             if *i == *j {
