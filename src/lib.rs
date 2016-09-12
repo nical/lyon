7916c5f8ca259147c0aa1c9aa049cc714a7392ca
Nicolas Silva <nical.silva@gmail.com>
2016-09-13 01:56:18 +0200

    Tag version 0.3.0.

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,12 @@ pub extern crate lyon_path_builder;
 pub extern crate lyon_path_iterator;
 pub extern crate lyon_tessellation;
 pub extern crate lyon_extra;
-pub extern crate lyon_svg;
+//pub extern crate lyon_svg;
 
 pub use lyon_core::*;
 pub use lyon_tessellation as tessellation;
 pub use lyon_path as path;
 pub use lyon_path_builder as path_builder;
 pub use lyon_path_iterator as path_iterator;
-pub use lyon_svg as svg;
+//pub use lyon_svg as svg;
 pub use lyon_extra as extra;
