0ddd2d5f18c214c17a180b086e401bc6f34796f8
Nicolas Silva <nical@fastmail.com>
2017-05-21 16:20:37 +0200

    Remove dependency to lyon_renderer in the basic example.

--- examples/gfx_basic/src/main.rs
+++ examples/gfx_basic/src/main.rs
@@ -4,7 +4,6 @@ extern crate gfx_window_glutin;
 extern crate gfx_device_gl;
 extern crate glutin;
 extern crate lyon;
-extern crate lyon_renderer;
 
 use lyon::extra::rust_logo::build_logo_path;
 use lyon::path_builder::*;
