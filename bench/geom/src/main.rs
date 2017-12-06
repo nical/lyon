ab468400cbb6aff1d500f526e84730c65d4c4b54
Nicolas Silva <nical@fastmail.com>
2017-12-06 18:59:31 +0100

    Add a simple benchmark for monotonic curve intersections.

---
+++ bench/geom/src/main.rs
@@ -0,0 +1,57 @@
+extern crate lyon;
+#[macro_use]
+extern crate bencher;
+
+use bencher::Bencher;
+
+use lyon::math::*;
+use lyon::geom::{QuadraticBezierSegment, monotonic_segment_intersecion};
+
+const N: usize = 10;
+
+fn monotonic_intersection(bench: &mut Bencher) {
+    // TODO: bench a variety of curves
+    let c1 = QuadraticBezierSegment {
+        from: point(10.0, 0.0),
+        ctrl: point(10.0, 90.0),
+        to: point(100.0, 90.0),
+    }.assume_monotonic();
+    let c2 = QuadraticBezierSegment {
+        from: point(0.0, 10.0),
+        ctrl: point(90.0, 10.0),
+        to: point(90.0, 100.0),
+    }.assume_monotonic();
+
+    bench.iter(|| {
+        for _ in 0..N {
+            monotonic_segment_intersecion(
+                &c1, 0.0..1.0,
+                &c2, 0.0..1.0,
+                0.001,
+            );
+            monotonic_segment_intersecion(
+                &c1, 0.0..0.5,
+                &c2, 0.0..0.5,
+                0.001,
+            );
+            monotonic_segment_intersecion(
+                &c1, 0.5..1.0,
+                &c2, 0.5..1.0,
+                0.001,
+            );
+            monotonic_segment_intersecion(
+                &c1, 0.3..0.7,
+                &c2, 0.3..0.7,
+                0.001,
+            );
+        }
+    });
+}
+
+benchmark_group!(intersections,
+  monotonic_intersection
+);
+
+benchmark_main!(
+    intersections
+);
