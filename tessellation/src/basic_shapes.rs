102d1289b2120e97e961de4ffcd3fd00f1d01929
Nicolas Silva <nical@fastmail.com>
2017-12-28 00:06:36 +0100

    Fix rounded rectangle stroke tessellator bug.

--- tessellation/src/basic_shapes.rs
+++ tessellation/src/basic_shapes.rs
@@ -337,6 +337,10 @@ pub fn fill_rounded_rectangle<Output: GeometryBuilder<FillVertex>>(
     let p1 = point(x_min + tl, y_min);
     let p2 = point(x_max - tr, y_min);
 
+    // right
+    let p3 = point(x_max, y_min + tr);
+    let p4 = point(x_max, y_max - br);
+
     // bottom
     let p6 = point(x_min + bl, y_max);
     let p5 = point(x_max - br, y_max);
@@ -345,15 +349,10 @@ pub fn fill_rounded_rectangle<Output: GeometryBuilder<FillVertex>>(
     let p0 = point(x_min, y_min + tl);
     let p7 = point(x_min, y_max - bl);
 
-    // right
-    let p3 = point(x_max, y_min + tr);
-    let p4 = point(x_max, y_max - br);
-
     let up = vec2(0.0, -1.0);
     let down = vec2(0.0, 1.0);
     let left = vec2(-1.0, 0.0);
     let right = vec2(1.0, 0.0);
-
 
     let v = [
         output.add_vertex(FillVertex { position: p7, normal: left }),
@@ -507,6 +506,10 @@ pub fn stroke_rounded_rectangle<Output: GeometryBuilder<StrokeVertex>>(
     let p1 = point(x_min + tl, y_min);
     let p2 = point(x_max - tr, y_min);
 
+    // right
+    let p3 = point(x_max, y_min + tr);
+    let p4 = point(x_max, y_max - br);
+
     // bottom
     let p6 = point(x_min + bl, y_max);
     let p5 = point(x_max - br, y_max);
@@ -515,15 +518,11 @@ pub fn stroke_rounded_rectangle<Output: GeometryBuilder<StrokeVertex>>(
     let p0 = point(x_min, y_min + tl);
     let p7 = point(x_min, y_max - bl);
 
-    // right
-    let p3 = point(x_max, y_min + tr);
-    let p4 = point(x_max, y_max - br);
-
     let sides = &[
         [p1, p2],
-        [p5, p6],
         [p3, p4],
-        [p0, p7],
+        [p5, p6],
+        [p7, p0],
     ];
 
     let radii = [tl, tr, br, bl];
@@ -551,9 +550,9 @@ pub fn stroke_rounded_rectangle<Output: GeometryBuilder<StrokeVertex>>(
         }
     });
 
-    { // output borrow scope start
+    {
         let mut builder = StrokeBuilder::new(options, output);
-        builder.move_to(p7);
+        builder.move_to(p0);
         for i in 0..4 {
             stroke_border_radius(
                 centers[i],
@@ -562,10 +561,12 @@ pub fn stroke_rounded_rectangle<Output: GeometryBuilder<StrokeVertex>>(
                 nums.next().unwrap(),
                 &mut builder,
             );
+
             builder.line_to(sides[i][0]);
             builder.line_to(sides[i][1]);
         }
-    } // output borrow scope end
+        builder.close();
+    }
 
     return output.end_geometry();
 }
