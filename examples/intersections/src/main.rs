6349307ca2381ca3ac8bc652cbc3b9c0bcc5341e
whmountains <8680147+whmountains@users.noreply.github.com>
2018-02-11 11:56:44 -0300

    Fix/update intersections example

--- examples/intersections/src/main.rs
+++ examples/intersections/src/main.rs
@@ -242,7 +242,7 @@ fn main() {
 
     while update_inputs(&mut events_loop, &mut scene) {
         gfx_window_glutin::update_views(&window, &mut main_fbo, &mut main_depth);
-        let (w, h) = window.get_inner_size_pixels().unwrap();
+        let (w, h) = window.get_inner_size().unwrap();
         scene.window_size = (w as f32, h as f32);
 
         cmd_queue.clear(&main_fbo.clone(), [0.0, 0.0, 0.0, 0.0]);
@@ -542,7 +542,7 @@ fn update_inputs(events_loop: &mut EventsLoop, scene: &mut SceneParams) -> bool 
                 );
             }
             Event::WindowEvent {
-                event: WindowEvent::MouseMoved {
+                event: WindowEvent::CursorMoved {
                     position: (x, y),
                     ..},
             ..} => {
