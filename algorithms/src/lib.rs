d6a1c6ed7e30e184cec2be371e936fd06c693be2
Nicolas Silva <nical@fastmail.com>
2018-06-09 21:41:14 +0200

    Implement basic hatching.

---
+++ algorithms/src/lib.rs
@@ -0,0 +1,13 @@
+#![doc(html_logo_url = "https://nical.github.io/lyon-doc/lyon-logo.svg")]
+
+//! 2D Path transformation and manipulation algorithms.
+//!
+
+// TODO doc!
+
+pub extern crate lyon_path as path;
+
+pub mod hatching;
+
+pub use path::math;
+pub use path::geom;
