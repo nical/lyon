145e4ab23322d49fd4a082ed734c12d8d8a8a24b
Nicolas Silva <nical@fastmail.com>
2018-07-05 00:50:31 +0200

    Version 0.11.0.

--- algorithms/src/lib.rs
+++ algorithms/src/lib.rs
@@ -1,6 +1,6 @@
 #![doc(html_logo_url = "https://nical.github.io/lyon-doc/lyon-logo.svg")]
 
-//! 2D Path transformation and manipulation algorithms.
+//! 2d Path transformation and manipulation algorithms.
 //!
 
 // TODO doc!
