7f2139660c9d6889e47e16d55437a1b746ae1873
Mark Wainwright <wainwrightml@gmail.com>
2023-01-04 10:52:38 +0000

    Added tests for rounded polygon

--- crates/algorithms/src/rounded_polygon.rs
+++ crates/algorithms/src/rounded_polygon.rs
@@ -31,7 +31,7 @@ pub fn add_rounded_polygon<B: PathBuilder>(
         let (q_current, q_next) = get_line_points(p_current, p_next, radius);
 
         let turn_winding = get_winding(p_previous, p_current, p_next);
-        arc_to(
+        arc(
             builder,
             Vector::new(radius, radius),
             Angle { radians: 0.0 },
@@ -71,7 +71,7 @@ fn get_winding(p0: Point, p1: Point, p2: Point) -> Winding {
     }
 }
 
-fn arc_to<B: PathBuilder>(
+fn arc<B: PathBuilder>(
     builder: &mut B,
     radii: Vector<f32>,
     x_rotation: Angle<f32>,
@@ -95,5 +95,98 @@ fn arc_to<B: PathBuilder>(
         geom_arc.for_each_quadratic_bezier(&mut |curve| {
             builder.quadratic_bezier_to(curve.ctrl, curve.to, attributes);
         });
+    }
+}
+
+#[test]
+fn rounded_polygon() {
+    use crate::geom::point;
+    use crate::rounded_polygon::*;
+    use alloc::vec::Vec;
+    use euclid::approxeq::ApproxEq;
+
+    type Point = euclid::Point2D<f32, euclid::UnknownUnit>;
+    type Event = path::Event<Point, Point>;
+    let arrow_points = [
+        point(-1.0, -0.3),
+        point(0.0, -0.3),
+        point(0.0, -1.0),
+        point(1.5, 0.0),
+        point(0.0, 1.0),
+        point(0.0, 0.3),
+        point(-1.0, 0.3),
+    ];
+
+    let arrow_polygon = Polygon {
+        points: &arrow_points,
+        closed: true,
+    };
+
+    let mut builder = lyon_path::Path::builder();
+    add_rounded_polygon(&mut builder, arrow_polygon, 2.0, lyon_path::NO_ATTRIBUTES);
+    let arrow_path = builder.build();
+
+    //check that we have the right ordering of event types
+    let actual_events: alloc::vec::Vec<_> = arrow_path.into_iter().collect();
+
+    let actual_event_types = actual_events
+        .iter()
+        .map(|x| match x {
+            Event::Begin { at: _ } => "b",
+            Event::Line { from: _, to: _ } => "l",
+            Event::Quadratic {
+                from: _,
+                ctrl: _,
+                to: _,
+            } => "q",
+            Event::Cubic {
+                from: _,
+                ctrl1: _,
+                ctrl2: _,
+                to: _,
+            } => "c",
+            Event::End {
+                last: _,
+                first: _,
+                close: _,
+            } => "e",
+        })
+        .collect::<alloc::vec::Vec<_>>()
+        .concat();
+
+    assert_eq!(actual_event_types, "bqqqlqqlqqlqqlqqlqqlqqle");
+
+    let expected_lines = std::vec![
+        (point(1.0, -0.3), point(-2.0, -0.3)),
+        (point(0.0, -2.3), point(0.0, 1.0)),
+        (point(1.66, 0.11), point(-0.16, -1.11)),
+        (point(-0.16, 1.11), point(1.66, -0.11)),
+        (point(-0.0, -1.0), point(0.0, 2.3)),
+        (point(-2.0, 0.3), point(1.0, 0.3)),
+        (point(-1.0, -1.7), point(-1.0, 1.7))
+    ];
+
+    //Check that the lines are approximately correct
+    let actual_lines: Vec<_> = arrow_path
+        .into_iter()
+        .filter_map(|event| match event {
+            Event::Line { from, to } => Some((from, to)),
+            _ => None,
+        })
+        .collect();
+
+    for (actual, expected) in actual_lines.into_iter().zip(expected_lines.into_iter()) {
+        for (actual_point, expected_point) in [(actual.0, expected.0), (actual.1, expected.1)] {
+            assert!(actual_point.approx_eq_eps(&expected_point, &Point::new(0.01, 0.01)))
+        }
+    }
+
+    //Check that each event goes from the end of the previous event
+
+    let mut previous = actual_events[0].to();
+
+    for e in actual_events {
+        e.from().approx_eq(&previous);
+        previous = e.to();
     }
 }
